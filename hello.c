3e5829fd9b36b34507acfebab6d684356155db6b
christianemmert <christianemmert@icloud.com>
2021-03-16 19:10:17 +0100

    Love mouse

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+    int i;
+
+    for (i = 1; i < 10; i++) {
+        printf ("Love you mouse %d mal mehr als du mich! \n", i);
+    }
+
+    printf ("Hello World");
+    return 0;
+}
